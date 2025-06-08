623e3faad316a525ef9e7853eefa1e37785eb8f7
FurioStyle <benjamin.bglh@gmail.com>
2025-06-08 12:24:08 -0400

    feat: botones que interactuan al presionarlos agregando el valor de las monedas que representan al contador interno, agregados metodos para hacerlo posible

--- java/resources/PanelMonedas.java
+++ java/resources/PanelMonedas.java
@@ -1,6 +1,7 @@
 package resources;
 
-import Logica.Monedas.Moneda;
+import Logica.Monedas.*;
+
 
 import javax.swing.*;
 import java.awt.*;
@@ -9,24 +10,55 @@ import java.util.List;
 public class PanelMonedas extends JPanel {
     private DefaultListModel<String> monedasModel;
     private JList<String> monedasList;
+    private int saldo;
+    private JLabel dineroLabel;
+    private Separador separador = new Separador();
 
     public PanelMonedas(List<Moneda> monedas) {
         setLayout(new BorderLayout());
-        setBorder(BorderFactory.createTitledBorder("Logica.Monedas (Vuelto)"));
-
+        setBorder(BorderFactory.createTitledBorder("Monedas"));
         monedasModel = new DefaultListModel<>();
         for (Moneda m : monedas) {
             monedasModel.addElement("Moneda: $" + m.getValor());
         }
 
         monedasList = new JList<>(monedasModel);
-        add(new JScrollPane(monedasList), BorderLayout.CENTER);
+        add(new JScrollPane(monedasList), BorderLayout.EAST);
+
+
+        JPanel botonesPanel = new JPanel();
+        botonesPanel.setLayout(new GridLayout(5, 1, 5, 5));
+
+        dineroLabel = new JLabel("Saldo Disponible:" + getSaldo());
+        JButton boton1 = new JButton("100");
+        JButton boton2 = new JButton("500");
+        JButton boton3 = new JButton("1000");
+
+        botonesPanel.add(dineroLabel);
+        botonesPanel.add(separador.getLabel());
+        botonesPanel.add(boton1);
+        botonesPanel.add(boton2);
+        botonesPanel.add(boton3);
+
+        boton1.addActionListener(e -> agregarMoneda(new Moneda100()));
+        boton2.addActionListener(e -> agregarMoneda(new Moneda500()));
+        boton3.addActionListener(e -> agregarMoneda(new Moneda1000()));
+
+        add(botonesPanel, BorderLayout.EAST);
+
+
     }
 
-    public void actualizarMonedas(List<Moneda> monedas) {
-        monedasModel.clear();
-        for (Moneda m : monedas) {
-            monedasModel.addElement("Moneda: $" + m.getValor());
-        }
+    public void agregarMoneda(Moneda m){
+        this.saldo += m.getValor();
+        actualizarMonedas();
+    }
+
+    public void actualizarMonedas() {
+        dineroLabel.setText("Saldo Disponible:" + getSaldo());
+    }
+
+    public int getSaldo(){
+        return saldo;
     }
 }
