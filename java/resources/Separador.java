58aafeca089bb6204d85ae0c6acf82a8219fa5e0
FurioStyle <benjamin.bglh@gmail.com>
2025-06-09 11:05:32 -0400

    docs: documentacion completa

--- java/resources/Separador.java
+++ java/resources/Separador.java
@@ -5,10 +5,17 @@ import javax.swing.*;
 public class Separador {
     private JLabel separador;
 
+    /**
+     * Separador de texto generico, creado por comodidad.
+     */
     public Separador(){
         separador = new JLabel("--------------------------------------------------");
     }
 
+    /**
+     * Getter de Label
+     * @return JLabel con el separador
+     */
     public JLabel getLabel(){
         return separador;
     }
