a1b989fc70768ed9ac7c84fce33165b64e10f22b
FurioStyle <benjamin.bglh@gmail.com>
2025-06-08 23:29:54 -0400

    fix: main solo creara la ventana

--- java/Logica/Main.java
+++ java/Logica/Main.java
@@ -1,48 +1,9 @@
 package Logica;
 
-import Logica.Excepciones.NoHayProductoException;
-import Logica.Excepciones.PagoIncorrectoException;
-import Logica.Monedas.*;
-
+import resources.VentanaPrincipal;
 
 public class Main {
-    public static void main(String[] args) throws NoHayProductoException, PagoIncorrectoException {
-        Moneda m = null;
-        Comprador c = null;
-        m = new Moneda1000();
-        // Elija un articulo
-        // 1 = CocaCola
-        c = new Comprador(m, 1);
-        System.out.println(c.queCompraste()+", "+c.cuantoVuelto());
-        // 2 = Sprite
-        c = new Comprador(m, 2);
-        System.out.println(c.queCompraste()+", "+c.cuantoVuelto());
-        // 3 = Fanta
-        c = new Comprador(m, 3);
-        System.out.println(c.queCompraste()+", "+c.cuantoVuelto());
-        // 4 = Super8
-        c = new Comprador(m, 4);
-        System.out.println(c.queCompraste()+", "+c.cuantoVuelto());
-        // 5 = Snickers
-        c = new Comprador(m, 5);
-        System.out.println(c.queCompraste()+", "+c.cuantoVuelto());
-
-        // Logica.Excepciones
-
-        // No hay dinero suficiente
-
-        m = new Moneda100();
-        c = new Comprador(m,1);
-
-        // Numero Invalido
-
-        m = new Moneda1000();
-        c = new Comprador(m,7);
-
-        //Moneda Nula
-
-        m = null;
-        c = new Comprador(m,1);
+    public static void main(String[] args) {
+        VentanaPrincipal v = new VentanaPrincipal();
     }
 }
-
