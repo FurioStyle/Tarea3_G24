547594003db5308322e9b9c765e49af477c9a5cb
FurioStyle <benjamin.bglh@gmail.com>
2025-06-09 11:05:00 -0400

    docs: documentacion completa

--- java/resources/PanelComprador.java
+++ java/resources/PanelComprador.java
@@ -7,6 +7,9 @@ public class PanelComprador extends JPanel {
     private JLabel productoLabel;
     private JLabel vueltoLabel;
 
+    /**
+     * Crea un nuevo Panel con texto, donde se muestran el producto comprado y el vuelto recibido.
+     */
     public PanelComprador() {
         setLayout(new GridLayout(2, 1));
         setBorder(BorderFactory.createTitledBorder("Comprador"));
@@ -18,6 +21,12 @@ public class PanelComprador extends JPanel {
         add(vueltoLabel);
     }
 
+    /**
+     * Este metodo actualiza lo mostrado en el panel de comprador, muestra lo que se compró y el vuelto recibido,
+     * también, añade el vuelto al saldo disponible.
+     * @param comprador comprador, para saber que compro y cuanto vuelto necesita.
+     * @param panelMonedas el panel donde se administra todo lo del saldo, para actualizar el saldo disponible.
+     */
     public void actualizarComprador(Comprador comprador, PanelMonedas panelMonedas) {
         productoLabel.setText("Producto comprado: " + comprador.queCompraste());
         int totalVuelto = comprador.cuantoVuelto();
@@ -25,6 +34,9 @@ public class PanelComprador extends JPanel {
         vueltoLabel.setText("Vuelto recibido: $" + totalVuelto);
     }
 
+    /**
+     * Este metodo representa como texto en el panel comprador, cuando se debe retirar el producto.
+     */
     public void empuja(){
         productoLabel.setText("Producto comprado: Retírelo" );
     }
